d9af2b9b16db12a06d9a30cab11f4565dededa01
Richard <RichardLiew@163.com>
2023-03-20 11:17:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -108,7 +108,6 @@ sudo yum install -y yum-utils
 sudo yum-config-manager \
     --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 sudo yum-config-manager --enable docker-ce-nightly
-sudo yum makecache fast
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 
 ################################################################################
